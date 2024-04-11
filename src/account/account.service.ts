d52007b9059b1f829bb0cf8244acc161fe4d054e
Avishkar <gaikwadavishkar546@gmail.com>
2024-04-11 09:56:14 +0530

    Designed API-4 Balance API

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,8 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Account } from './account.entity';
 
 @Injectable()
 export class AccountService {
+  constructor(
+    @InjectRepository(Account)
+    private accountRepository: Repository<Account>,
+  ) {}
   topUpAccount(currency: string, amount: number): string {
     return `Successfully topped up ${amount} ${currency}`;
   }
-}   
+
+  async getAccountBalances(
+    userId: string,
+  ): Promise<{ [currency: string]: number }> {
+    const account = await this.accountRepository.findOne({
+      where: { userId },
+      relations: ['balances'],
+    });
+
+    if (!account) {
+      throw new Error('User account not found');
+    }
+
+    // Extract balances for each currency
+    const balances = {};
+    account.balances.forEach((balance) => {
+      balances[balance.currency] = balance.amount;
+    });
+
+    return balances;
+  }
+}
